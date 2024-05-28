694a79183961b0e059f71be85e63e83e4c4b1553
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-05-28 19:18:09 +0000

    feat: clase1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cohorte = "Spain-72";
+  let age = 26;
+
+  let person1 = {
+    name: "Joe",
+    age: 35,
+    lastname: "Doe",
+    sports: ["Futbol", "Running", "Atletismo"]
+  };
+  let person2 = {
+    name: "Jane",
+    age: 27,
+    lastname: "Castillo",
+    sports: ["Tenis", "baloncesto", "Volley"]
+  };
+  let person3 = {
+    name: "Jimmy",
+    age: 10,
+    lastname: "Martinez",
+    sports: ["Rugby", "Beisball", "Natación"]
+  };
+
+  let family = [person1, person2, person3];
+  let personas = "";
+  let tag = document.querySelector("#persons");
+  family.map((item, index) => {
+    personas +=
+      "<li class='list-group-item'>" +
+      item.name +
+      " " +
+      item.lastname +
+      "</li>";
+  });
+
+  tag.innerHTML = personas;
+  console.log(tag.innerHTML);
 };
